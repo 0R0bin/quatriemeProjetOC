83463853ab044221933c6f0b3db5c5a046d74320
Robin <robinsenechal0@gmail.com>
2022-07-25 10:36:41 +0200

    last info tournament + add tours & matchs

--- models/tournament.py
+++ models/tournament.py
@@ -3,9 +3,13 @@
 class Tournament:
     """Tournament"""
 
-    def __init__(self, name, place, date, tours):
+    def __init__(self, name, place, date, rounds, players, timeplay, description, nb_rounds=4):
         "Some informations about the tournament"
         self.name = name
         self.place = place
         self.date = date
-        self.tours = tours
+        self.rounds = rounds
+        self.players = players
+        self.timeplay = timeplay
+        self.description = description
+        self.nb_rounds = nb_rounds
