911389996f55d13f825e7af81c5641c8747d33e6
Robin <robinsenechal0@gmail.com>
2022-08-02 11:09:11 +0200

    add classes (commit before holidays)

---
+++ models/match.py
@@ -0,0 +1,11 @@
+"""Define the match"""
+
+class Match:
+    """Match"""
+
+    def __init__(self, players):
+        "Some informations about the Match"
+        self.name = players
+
+    def randomnWinner(self):
+        "Chose a random winner between the two players"
---
+++ models/tours.py
@@ -0,0 +1,11 @@
+"""Define the tours"""
+
+class Tours:
+    """Tours"""
+
+    def __init__(self, name, matchs, date_debut, date_fin):
+        "Some informations about the tours"
+        self.name = name
+        self.name = matchs
+        self.date_debut = date_debut
+        self.date_fin = date_fin
